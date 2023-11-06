581a8eca190456d33f8a50ea02e25f6c75ee1736
Zinedin Kadric <zinedin_kadric@hotmail.com>
2023-11-06 15:06:26 +0100

    Finished functions for solution validation

--- src/views/main-view.js
+++ src/views/main-view.js
@@ -128,17 +128,66 @@ const tempSolution = [
   oneCellInSolution3,
 ];
 
-// Function to check if some professor is in different classes at the same time
-// Solution is made of 'solutionCells', we have to check if some two cells have the
-// the same attributes professorIdx and timeIdx which would mean that the same professor
-// is asigned to different class at the same time
+/////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////    HELPER FUNCTIONS    /////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+//  Function that checks if array has breaks between class times
+
+const checkIfClassDayIsContinous = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i + 1][0] == arr[i][0]) {
+      // If its still the same day m == m for example (monday == monday)
+      if (arr[i + 1][1] - arr[i][1] != 1) {
+        return false;
+      } else {
+        continue;
+      }
+    } else {
+      continue;
+    }
+  }
+};
+
+const checkIfProfessorDayIsContinousOrWithOneBreak = (arr) => {
+  let foundOneBreakInOneDay = false;
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i + 1][0] == arr[i][0]) {
+      // If its still the same day m == m for example (monday == monday)
+      if (arr[i + 1][1] - arr[i][1] > 1) {
+        return false;
+      } else if (arr[i + 1][1] - arr[i][1] == 1) {
+        if (foundOneBreakInOneDay === true) {
+          //  We found another break in one day
+          return false;
+        }
+        foundOneBreakInOneDay = true;
+        continue;
+      } else {
+        continue;
+      }
+    } else {
+      foundOneBreakInOneDay = false; // We switched from one day to another so we need to restart the variable for checking one break a day
+      continue;
+    }
+  }
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//  Function to check if some professor is in different classes at the same time
+//  Solution is made of 'solutionCells', we have to check if some two cells have the
+//  the same attributes professorIdx and timeIdx which would mean that the same professor
+//  is asigned to different class at the same time
 const checkForSameProfessorDifferentClass = (solution) => {
-  // we will iterate through the solution(all cells) for all professors and check if the times collide
+  //    we will iterate through the solution(all cells) for all professors and check if the times collide
   for (let i = 0; i < professors.length; i++) {
     let timesForProfessor = new Array(time.length).fill(false);
     for (s in solution) {
       if (s.professorIdx === i && timesForProfessor[s.timeIdx] === true) {
-        return false; // We found a cell in which is professor assigned to a time in which he is already assigned
+        return false; //    We found a cell in which is professor assigned to a time in which he is already assigned
       } else if (
         s.professorIdx === i &&
         timesForProfessor[s.timeIdx] === false
@@ -149,7 +198,99 @@ const checkForSameProfessorDifferentClass = (solution) => {
       }
     }
   }
-  return true; // We didnt find double times for the same professor
+  return true; //   We didnt find double times for the same professor
+};
+
+//  Function checks if some class has all their subjects one after another,
+//  without time gaps of 45mins or more and have between 4 and 7 classes a day
+const checkClassGapsAndNumOfLessons = (solution) => {
+  //    we will iterate through the solution(all cells) for all classes and check if one class has continous lessons in a day
+  for (let i = 0; i < classes.length; i++) {
+    let classesPerDay = new Array(5).fill(0); //    Array of zeros for each day from monday to friday, when we find a solution cell for we increment that index in an array
+    let allClassTimesFound = new Array(); // We will fill this array with all times found for current class in current solution, then sort it and check if the times for each day are continous
+    //  Could optimize this to make it instead of going through the solution for each class, check for all classes in one going by putting
+    //  variables 'classesPerDay and allClassTimesFound in an array, where array is slot for each class to check
+    for (s in solution) {
+      if (s.classIdx === i) {
+        if (time[s.timeIdx][0] == "m") {
+          classesPerDay[0]++;
+          allClassTimesFound.push(time[s.timeIdx]);
+        } else if (time[s.timeIdx][0] == "t") {
+          if (time[s.timeIdx][1] != "h") {
+            classesPerDay[1]++;
+            allClassTimesFound.push(time[s.timeIdx]);
+          } else {
+            classesPerDay[3]++;
+            allClassTimesFound.push(time[s.timeIdx]);
+          }
+        } else if (time[s.timeIdx][0] == "w") {
+          classesPerDay[2]++;
+          allClassTimesFound.push(time[s.timeIdx]);
+        } else if (time[s.timeIdx][0] == "f") {
+          classesPerDay[4]++;
+          allClassTimesFound.push(time[s.timeIdx]);
+        }
+      } else {
+        continue;
+      }
+    }
+    for (let j = 0; j < 5; j++) {
+      if (classesPerDay < 4 || classesPerDay > 7) return false;
+    }
+    allClassTimesFound.sort();
+    if (checkIfClassDayIsContinous(allClassTimesFound) == false) {
+      return false;
+    }
+  }
+  return true; //   We didnt find double times for the same professor
+};
+
+//  Functions that checks if professor has at most one break of 45 mins,
+//  a 'one lesson' gap and between 2 and 7 classes a day
+const checkProfessorBreakAndNumOfLessons = (solution) => {
+  //    we will iterate through the solution(all cells) for all professors and check if one professor has at most
+  //    1 break of 45 mins between classes in a day and has between 2 and 7 lessons a day
+  for (let i = 0; i < professors.length; i++) {
+    let classesPerDay = new Array(5).fill(0); //    Array of zeros for each day from monday to friday, when we find a solution cell for we increment that index in an array
+    let allClassTimesFound = new Array(); // We will fill this array with all times found for current professor in current solution, then sort it and check if the times for each day are continous
+    //  or with one break at most
+    //  Could optimize this to make it instead of going through the solution for each professor, check for all professor in one going by putting
+    //  variables 'classesPerDay and allClassTimesFound in an array, where array is slot for each class to check
+    for (s in solution) {
+      if (s.professorIdx === i) {
+        if (time[s.timeIdx][0] == "m") {
+          classesPerDay[0]++;
+          allClassTimesFound.push(time[s.timeIdx]);
+        } else if (time[s.timeIdx][0] == "t") {
+          if (time[s.timeIdx][1] != "h") {
+            classesPerDay[1]++;
+            allClassTimesFound.push(time[s.timeIdx]);
+          } else {
+            classesPerDay[3]++;
+            allClassTimesFound.push(time[s.timeIdx]);
+          }
+        } else if (time[s.timeIdx][0] == "w") {
+          classesPerDay[2]++;
+          allClassTimesFound.push(time[s.timeIdx]);
+        } else if (time[s.timeIdx][0] == "f") {
+          classesPerDay[4]++;
+          allClassTimesFound.push(time[s.timeIdx]);
+        }
+      } else {
+        continue;
+      }
+    }
+    for (let j = 0; j < 5; j++) {
+      if (classesPerDay < 2 || classesPerDay > 7) return false;
+    }
+    allClassTimesFound.sort();
+    if (
+      checkIfProfessorDayIsContinousOrWithOneBreak(allClassTimesFound) == false
+    ) {
+      return false;
+    }
+  }
+  return true; //   We didnt find double times for the same professor
 };
 
 export default function MainView() {
