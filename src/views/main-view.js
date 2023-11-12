409f8c0c18d34ed1f0b68fec53d2a1b7aaf3d524
nihadm97 <nihadm97@outlook.com>
2023-11-12 20:34:04 +0100

    added batAlgorithm with helping functions getRdn, checkBound and average, cost function is temporary and needs to be replaced

--- src/views/main-view.js
+++ src/views/main-view.js
@@ -6,6 +6,9 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+
 
 const professors = [
   "Hozo Minja",
@@ -154,17 +157,66 @@ const tempSolution = [
   oneCellInSolution3,
 ];
 
-// Function to check if some professor is in different classes at the same time
-// Solution is made of 'solutionCells', we have to check if some two cells have the
-// the same attributes professorIdx and timeIdx which would mean that the same professor
-// is asigned to different class at the same time
+/////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////    HELPER FUNCTIONS    /////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+//  Function that checks if array has breaks between class times
+
+const checkIfClassDayIsContinous = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i + 1][0] == arr[i][0]) {
+      // If its still the same day M == M for example (monday == monday)
+      if (arr[i + 1][1] - arr[i][1] != 1) {
+        return false;
+      } else {
+        continue;
+      }
+    } else {
+      continue;
+    }
+  }
+};
+
+const checkIfProfessorDayIsContinousOrWithOneBreak = (arr) => {
+  let foundOneBreakInOneDay = false;
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i + 1][0] == arr[i][0]) {
+      // If its still the same day m == m for example (monday == monday)
+      if (arr[i + 1][1] - arr[i][1] > 1) {
+        return false;
+      } else if (arr[i + 1][1] - arr[i][1] == 1) {
+        if (foundOneBreakInOneDay === true) {
+          //  We found another break in one day
+          return false;
+        }
+        foundOneBreakInOneDay = true;
+        continue;
+      } else {
+        continue;
+      }
+    } else {
+      foundOneBreakInOneDay = false; // We switched from one day to another so we need to restart the variable for checking one break a day
+      continue;
+    }
+  }
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//  Function to check if some professor is in different classes at the same time
+//  Solution is made of 'solutionCells', we have to check if some two cells have the
+//  the same attributes professorIdx and timeIdx which would mean that the same professor
+//  is asigned to different class at the same time
 const checkForSameProfessorDifferentClass = (solution) => {
-  // we will iterate through the solution(all cells) for all professors and check if the times collide
+  //    we will iterate through the solution(all cells) for all professors and check if the times collide
   for (let i = 0; i < professors.length; i++) {
     let timesForProfessor = new Array(time.length).fill(false);
     for (s in solution) {
       if (s.professorIdx === i && timesForProfessor[s.timeIdx] === true) {
-        return false; // We found a cell in which is professor assigned to a time in which he is already assigned
+        return false; //    We found a cell in which is professor assigned to a time in which he is already assigned
       } else if (
         s.professorIdx === i &&
         timesForProfessor[s.timeIdx] === false
@@ -175,57 +227,328 @@ const checkForSameProfessorDifferentClass = (solution) => {
       }
     }
   }
-  return true; // We didnt find double times for the same professor
+  return true; //   We didnt find double times for the same professor
 };
 
+//  Function checks if some class has all their subjects one after another,
+//  without time gaps of 45mins or more and have between 4 and 7 classes a day
+const checkClassGapsAndNumOfLessons = (solution) => {
+  //    we will iterate through the solution(all cells) for all classes and check if one class has continous lessons in a day
+  for (let i = 0; i < classes.length; i++) {
+    let classesPerDay = new Array(5).fill(0); //    Array of zeros for each day from monday to friday, when we find a solution cell for we increment that index in an array
+    let allClassTimesFound = new Array(); // We will fill this array with all times found for current class in current solution, then sort it and check if the times for each day are continous
+    //  Could optimize this to make it instead of going through the solution for each class, check for all classes in one going by putting
+    //  variables 'classesPerDay and allClassTimesFound in an array, where array is slot for each class to check
+    for (s in solution) {
+      if (s.classIdx === i) {
+        if (time[s.timeIdx][0] == "M") {
+          classesPerDay[0]++;
+          allClassTimesFound.push(time[s.timeIdx]);
+        } else if (time[s.timeIdx][0] == "T") {
+          if (time[s.timeIdx][1] != "H") {
+            classesPerDay[1]++;
+            allClassTimesFound.push(time[s.timeIdx]);
+          } else {
+            classesPerDay[3]++;
+            allClassTimesFound.push(time[s.timeIdx]);
+          }
+        } else if (time[s.timeIdx][0] == "W") {
+          classesPerDay[2]++;
+          allClassTimesFound.push(time[s.timeIdx]);
+        } else if (time[s.timeIdx][0] == "F") {
+          classesPerDay[4]++;
+          allClassTimesFound.push(time[s.timeIdx]);
+        }
+      } else {
+        continue;
+      }
+    }
+    for (let j = 0; j < 5; j++) {
+      if (classesPerDay < 4 || classesPerDay > 7) return false;
+    }
+    allClassTimesFound.sort();
+    if (checkIfClassDayIsContinous(allClassTimesFound) == false) {
+      return false;
+    }
+  }
+  return true; //   We didnt find double times for the same professor
+};
+
+//  Functions that checks if professor has at most one break of 45 mins,
+//  a 'one lesson' gap and between 2 and 7 classes a day
+const checkProfessorBreakAndNumOfLessons = (solution) => {
+  //    we will iterate through the solution(all cells) for all professors and check if one professor has at most
+  //    1 break of 45 mins between classes in a day and has between 2 and 7 lessons a day
+  for (let i = 0; i < professors.length; i++) {
+    let classesPerDay = new Array(5).fill(0); //    Array of zeros for each day from monday to friday, when we find a solution cell for we increment that index in an array
+    let allClassTimesFound = new Array(); // We will fill this array with all times found for current professor in current solution, then sort it and check if the times for each day are continous
+    //  or with one break at most
+    //  Could optimize this to make it instead of going through the solution for each professor, check for all professor in one going by putting
+    //  variables 'classesPerDay and allClassTimesFound in an array, where array is slot for each class to check
+    for (s in solution) {
+      if (s.professorIdx === i) {
+        if (time[s.timeIdx][0] == "M") {
+          classesPerDay[0]++;
+          allClassTimesFound.push(time[s.timeIdx]);
+        } else if (time[s.timeIdx][0] == "T") {
+          if (time[s.timeIdx][1] != "H") {
+            classesPerDay[1]++;
+            allClassTimesFound.push(time[s.timeIdx]);
+          } else {
+            classesPerDay[3]++;
+            allClassTimesFound.push(time[s.timeIdx]);
+          }
+        } else if (time[s.timeIdx][0] == "W") {
+          classesPerDay[2]++;
+          allClassTimesFound.push(time[s.timeIdx]);
+        } else if (time[s.timeIdx][0] == "F") {
+          classesPerDay[4]++;
+          allClassTimesFound.push(time[s.timeIdx]);
+        }
+      } else {
+        continue;
+      }
+    }
+    for (let j = 0; j < 5; j++) {
+      if (classesPerDay < 2 || classesPerDay > 7) return false;
+    }
+    allClassTimesFound.sort();
+    if (
+      checkIfProfessorDayIsContinousOrWithOneBreak(allClassTimesFound) == false
+    ) {
+      return false;
+    }
+  }
+  return true; //   We didnt find double times for the same professor
+};
+
+function getRdn(min, max) {
+	return Math.random() * (max - min) + min
+}
+
+function checkBound(val, lower, upper) {
+	if(val < lower) return lower;
+	if(val > upper) return upper;
+	return val;
+}
+
+function average(array) {
+	let sum = 0
+	for (let i = 0; i < array.length; i++) {
+		sum += array[i]
+	}
+	return sum / array.length
+}
+
+
+// Define the cost function
+function cost(x) { // temporaly cost function
+	let sum = 0
+	for (let i = 0; i < x.length; i++) {
+		sum += x[i].timeIdx * x[i].timeIdx
+	}
+	return sum
+}
+
+
+/*
+function cost(x) { // reference error in checkProfessorBreakAndNumOfLessons on Line 288, needs to be resolved
+	let sum = 0
+	for (let i = 0; i < x.length; i++) {
+		if(checkProfessorBreakAndNumOfLessons(x)){
+      sum += 5000;
+    }
+	}
+	return sum
+}
+*/
+
+function switchTimes(arrayTimes, arraySolution){ // switch old timeslots with new timeslots
+  for(let i=0; i < arrayTimes.length; i++){
+    arraySolution[i].timeIdx = arrayTimes[i];
+  }
+  return arraySolution;
+}
+
+// bat algorithm for finding best timeslots for tempSolution, rewrites the tempSolution with new timeslots
+// bestBat variable includes best timeslots for tempSolution
+function batAlgorithm(costFunc, saveRate = 100, maxGen = 1000, popSize = 50, solution = tempSolution, maxLoudness = 2, 
+                      maxPulseRate = 1, fMin = 0, fMax = 10, lowerBound = 0, upperBound = time.length) {
+
+	if (!costFunc) throw new Error("Please pass a valid cost function for your optimization problems");
+		const ALPHA = 0.9;
+		const GAMMA = 0.9;
+
+		// parameters initialization using uniform distribution
+		let position = [];
+		let velocity = [];
+		let loudness = [];
+		let pulseRate = [];
+		let frequency = [];
+		let newPosition = []; // bats local array used to calculate bats new position at each generation, before acceptation
+
+		for (let i = 0; i < popSize; i++) {
+			loudness[i] = getRdn(1, maxLoudness);
+			pulseRate[i] = getRdn(0, maxPulseRate);
+			frequency[i] = getRdn(fMin, fMax);
+			position[i] = [];
+			velocity[i] = [];
+			newPosition[i] = [];
+
+			for (let j = 0; j < solution.length; j++) {
+				position[i][j] = Math.round(getRdn(lowerBound, upperBound));
+				velocity[i][j] = getRdn(lowerBound, upperBound);
+			}
+		}
+
+		// evaluate the bats after initialization
+		let cost = [];
+		for (let i = 0; i < popSize; i++) {
+			cost[i] = costFunc(switchTimes(position[i], solution));
+		}
+
+		let bestBat;
+		// cycle through each generation
+		for (let gen = 1; gen <= maxGen; gen++) {
+			let indexMin = cost.indexOf(Math.min(...cost)); // best bat index so far
+			bestBat = position[indexMin]; // best bat so far
+
+			if(gen % saveRate === 0 || gen === 1) {
+				let data = {};
+
+				data.gen = gen;
+				data.loudness = loudness;
+				data.pulseRate = pulseRate;
+				data.frequency = frequency;
+				data.position = position;
+				data.velocity = velocity;
+				data.bestBat = bestBat;
+				
+			}
+
+			// cycle through the population
+			for (let i = 0; i < popSize; i++) {
+				// getRdn(0,1) = beta
+				frequency[i] = fMin + (fMax - fMin) * getRdn(0,1); // assign new frequency to each bat (2)
+				for (let j = 0; j < solution.length; j++) {
+					velocity[i][j] = velocity[i][j] + (position[i][j] - bestBat[j]) * frequency[i]; // assign new velocity to each bat (3)
+					newPosition[i][j] = Math.round(position[i][j] + velocity[i][j]); // creating a new position that is not yet assigned to the bat (4)
+					newPosition[i][j] = checkBound(newPosition[i][j], lowerBound, upperBound);
+				}
+
+				// generate a local solution around the best solution
+				if(getRdn(0,1) > pulseRate[i]) {
+					for (let j = 0; j < solution.length; j++) {
+						// getRdn(-1, 1) = epsilon
+						newPosition[i][j] = Math.round(bestBat[j] + getRdn(-1,1) * average(loudness)); // random walk (5)
+						newPosition[i][j] = checkBound(newPosition[i][j], lowerBound, upperBound);
+					}
+				}
+
+				let newCost = costFunc(switchTimes(newPosition[i], solution)); // bat 'newPosition's cost
+
+				// try to accept the new solution
+				if(getRdn(0,1) < loudness[i] && newCost <= cost[i]) {
+					// new solution accepted, assigning new position to each bat
+					for (let j = 0; j < solution.length; j++) {
+						position[i][j] = newPosition[i][j];
+					}
+					cost[i] = newCost;
+					loudness[i] = loudness[i] * ALPHA; // loudness update (6)
+					pulseRate[i] = pulseRate[i] * (1 - Math.exp(-GAMMA * gen)); // pulse rate update (6)
+				}
+			}
+		}
+    switchTimes(bestBat, solution);
+		return(solution)
+	
+}
+
+// Call the batAlgorithm() function with optional parameters
+//const result = batAlgorithm(cost);
+const result = batAlgorithm(cost, 150, 10000, 100, tempSolution, 2, 1, -10, 10, 0, time.length);
+console.log(result);
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Roboto', 
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif'
+    ].join(','),
+  },
+
+});
 
 export default function MainView() {
+  const renderCell = (timeslotIndex, row) => {
+    const cellStyles = {
+      bgcolor: timeslotIndex === row.timeIdx ? 'secondary.main' : 'background.paper',
+      color: timeslotIndex === row.timeIdx ? 'common.white' : 'text.primary',
+      textAlign: 'center',
+      borderRight: timeslotIndex < time.length - 1 ? '1px solid rgba(224, 224, 224, 1)' : '',
+    };
 
+    return (
+      <TableCell key={`cell-${timeslotIndex}`} sx={cellStyles}>
+        {timeslotIndex === row.timeIdx && (
+          <>
+            {classes[row.classIdx]}
+            <br />
+            {classrooms[row.classroomIdx]}
+            <br />
+            {subjects[row.subjectIdx]}
+          </>
+        )}
+      </TableCell>
+    );
+  };
+
+  const tableCellStyle = {
+    fontWeight: 'bold',
+    bgcolor: 'primary.dark',
+    color: 'common.white',
+    textAlign: 'center',
+  };
+
+  const tableHeaderStyle = {
+    bgcolor: 'primary.main',
+    color: 'common.white',
+    textAlign: 'center',
+  };
+
+        
   return (
-    <Container>
-      <Box
-        sx={{ display: "flex", justifyContent: "center", marginTop: "70px" }}
-      ></Box>
-    <TableContainer component={Paper} style={{ paddingLeft: 0, paddingRight: 0, marginLeft:0, marginRight:0, width: '100%' }}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-          <TableCell>Professor</TableCell>
-          {time.map((timeslot) => (
-            <TableCell>{timeslot}</TableCell> // write all timeslot labels
-          ))}
-            
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {tempSolution.map((row) => (
-          <TableRow
-            key={row.professorIdx} 
-            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-          >
-          <TableCell component="th" scope="row">
-            {professors[row.professorIdx]}
-          </TableCell>
-          {time.map((index) => {
-            if (index === time[row.timeIdx]) { // when professor has a lecture in that timeslot visualize it 
-              return (
-                <TableCell align="center">
-                  {classes[row.classIdx]}
-                  {"\n"}
-                  {classrooms[row.classroomIdx]}
-                  {"\n"}
-                  {subjects[row.subjectIdx]}
-                </TableCell>
-        );
-      }
-      return <TableCell align="right"></TableCell>; // when professor does not have a lecture in that timeslot just create empty cell (necessary for correct visualization of lectures)
-          })}
-        </TableRow>
-        ))}
-
-        </TableBody>
-      </Table>
-      </TableContainer>
+    <ThemeProvider theme={theme}>
+    <Container sx={{ mt: '70px', pl: 0, width: '100%' }}>
+      <Box display="flex" justifyContent="center">
+        <TableContainer component={Paper} elevation={3}>
+          <Table sx={{ minWidth: 650 }} aria-label="schedule table">
+            <TableHead>
+              <TableRow>
+                <TableCell sx={tableHeaderStyle}>Professor</TableCell>
+                {time.map((timeslot, index) => (
+                  <TableCell key={`head-cell-${index}`} sx={tableHeaderStyle}>
+                    {timeslot}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {tempSolution.map((row, index) => (
+                <TableRow key={`row-${index}`} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row" sx={tableCellStyle}>
+                    {professors[row.professorIdx]}
+                  </TableCell>
+                  {time.map((_, timeslotIndex) => renderCell(timeslotIndex, row))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
     </Container>
+    </ThemeProvider>
   );
 }
