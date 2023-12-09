aca0c43022f2d8957ec513d24803daab83e0a82e
Adelisa Softic <softicadelisa842@gmail.com>
2023-12-09 21:58:36 +0100

    feat:popunjavanje rasporeda studentima

--- src/views/main-view.js
+++ src/views/main-view.js
@@ -1257,11 +1257,69 @@ const initialSolution1 = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8
 console.log(cost_2(initialSolution1)); // hardcoded solution has no gaps and 4 lessons every day but checkIfProfessorDayIsContinousOrWithOneBreak returns -5020
 
 
+function getAllProfessors(schedule) {
+  const professorIndices = new Set(); 
+
+  for (let cell of schedule) {
+      professorIndices.add(cell.professorIdx); 
+  }
+
+  return Array.from(professorIndices); 
+}
+
+function canMoveClass(schedule, classToMove, newTimeIdx) {
+  // Ovdje provjeravamo da li se trazeni cas moze pomjeriti unazad
+  // na novi vremenski indeks/termin
+  for (let otherClass of schedule) {
+    if (otherClass !== classToMove && otherClass.timeIdx === newTimeIdx) {
+        if (otherClass.classroomIdx === classToMove.classroomIdx || 
+            otherClass.classIdx === classToMove.classIdx) {
+            return false;
+        }
+    }
+  }
+  return true;
+}
+
+
+function optimizeScheduleByProfessor(initialSolution) {
+  const professors = getAllProfessors(initialSolution);
+  let schedule = [...initialSolution];
+
+  for (let professor of professors) {
+      let professorsClasses = schedule.filter(c => c.professorIdx === professor);
+      console.log(professorsClasses, professor)
+      // Sortiranje časova po  terminu
+      professorsClasses.sort((a, b) => a.timeIdx - b.timeIdx);
+
+      let earliestAvailableTime = 0;
+      for (let classToMove of professorsClasses) {
+          while (earliestAvailableTime < classToMove.timeIdx) {
+              // Pokusavamo vratiti cas unazad na ustrb profesora
+              // Ako je dat neki termin daleko, a studentima se moze ubaciti taj termin
+              // onda ga ubacimo u najraniji moguci
+              // Ovim smo osigurali da je raspored studentima prepunjen
+              if (canMoveClass(schedule, classToMove, earliestAvailableTime)) {
+                  classToMove.timeIdx = earliestAvailableTime;
+                  break;
+              }
+              earliestAvailableTime++;
+          }
+          // Updateovanje najranijeg slobodnog vremena za sljedeći čas
+          earliestAvailableTime = classToMove.timeIdx + 1;
+      }
+      console.log(professorsClasses, professor)
+  }
+
+  return schedule;
+}
+
+
 export default function MainView() {
 
-  const initialSolution = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9, cell10, cell11, cell12, cell13, cell14, cell15, cell16, cell17, cell18, cell19, cell20, cell21, cell22, cell23, cell24, cell25, cell26, cell27, cell28, cell29, cell30, cell31, cell32, cell33, cell34, cell35, cell36, cell37, cell38, cell39, cell40, cell41, cell42, cell43, cell44, cell45, cell46, cell47, cell48, cell49, cell50, cell51, cell52, cell53, cell54, cell55, cell56, cell57, cell58, cell59, cell60, cell61, cell62, cell63, cell64, cell65, cell66, cell67, cell68, cell69, cell70, cell71, cell72, cell73, cell74, cell75, cell76, cell77, cell78, cell79, cell80, cell81, cell82, cell83, cell84, cell85, cell86, cell87, cell88, cell89, cell90, cell91, cell92, cell93, cell94, cell95, cell96, cell97, cell98, cell99, cell100, cell101, cell102, cell103, cell104];
+  let initialSolution = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9, cell10, cell11, cell12, cell13, cell14, cell15, cell16, cell17, cell18, cell19, cell20, cell21, cell22, cell23, cell24, cell25, cell26, cell27, cell28, cell29, cell30, cell31, cell32, cell33, cell34, cell35, cell36, cell37, cell38, cell39, cell40, cell41, cell42, cell43, cell44, cell45, cell46, cell47, cell48, cell49, cell50, cell51, cell52, cell53, cell54, cell55, cell56, cell57, cell58, cell59, cell60, cell61, cell62, cell63, cell64, cell65, cell66, cell67, cell68, cell69, cell70, cell71, cell72, cell73, cell74, cell75, cell76, cell77, cell78, cell79, cell80, cell81, cell82, cell83, cell84, cell85, cell86, cell87, cell88, cell89, cell90, cell91, cell92, cell93, cell94, cell95, cell96, cell97, cell98, cell99, cell100, cell101, cell102, cell103, cell104];
   const [tempSolution, setTempSolution] = useState(initialSolution);
-
+  initialSolution = optimizeScheduleByProfessor(initialSolution);
   const groupedLessons = {};
   tempSolution.forEach(lesson => {
       const professorName = professors[lesson.professorIdx];
