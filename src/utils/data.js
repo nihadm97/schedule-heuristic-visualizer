7f37949dfcb99883a8e529d1c03f3954787caf1f
Zinedin Kadric <zinedin_kadric@hotmail.com>
2023-12-22 22:30:19 +0100

    Added cost functions and fixed the existing ones

---
+++ src/utils/data.js
@@ -0,0 +1,852 @@
+export const professors = [
+  "Hozo Minja",
+  "Babić Aida",
+  "Seno Čolo",
+  "Hujdur Azemina",
+  "Likić Merima",
+  "Zulčić Ankica",
+  "Karčić Muhidin",
+  "Kopić Edina",
+  "Mehmedović Nihad",
+  "Tadić Suada",
+];
+export const subjects = [
+  "BHS",
+  "ENG",
+  "NJEM",
+  "TZO",
+  "MAT",
+  "HIST",
+  "VJE",
+  "OZ",
+  "FIZ",
+  "GEO",
+  "HEM",
+  "DSPK",
+  "MAR",
+  "EKO",
+  "OSEKO",
+  "INFTEH",
+  "MEĐEKO",
+  "EUIMS",
+  "TBR",
+  "BAKPOS",
+  "PRAVO",
+  "UUPP",
+  "EKOMAT",
+  "PRAKSA",
+  "KNJI",
+  "PZV",
+];
+
+export const time = [
+  "M1",
+  "M2",
+  "M3",
+  "M4",
+  "M5",
+  "M6",
+  "TU1",
+  "TU2",
+  "TU3",
+  "TU4",
+  "TU5",
+  "TU6",
+  "W1",
+  "W2",
+  "W3",
+  "W4",
+  "W5",
+  "W6",
+  "TH1",
+  "TH2",
+  "TH3",
+  "TH4",
+  "TH5",
+  "TH6",
+  "F1",
+  "F2",
+  "F3",
+  "F4",
+  "F5",
+  "F6",
+];
+
+export const classrooms = [
+  "PR1",
+  "PR2",
+  "MULTI",
+  "P1",
+  "P2",
+  "P3",
+  "P4",
+  "P5",
+  "P6",
+  "D1",
+  "D2",
+  "D3",
+  "D4",
+  "D5",
+  "D6",
+  "T1",
+  "T2",
+  "T3",
+  "T4",
+  "T5",
+  "Sala",
+];
+
+export const classes = [
+  "I.1",
+  "I.2",
+  "I.3",
+  "I.4",
+  // "II.1",
+  // "II.2",
+  // "II.3",
+  // "II.4",
+  // "III.1",
+  // "III.2",
+  // "III.3",
+  // "III.4",
+  // "IV.1",
+  // "IV.2",
+  // "IV.3",
+  // "IV.4",
+];
+//  5-tuple consisting of 5 indices, first index represents professor,
+//  second index represents the time, third index is subject, fourth is classroom and
+//  fifth is class
+
+export const cell1 = {
+  professorIdx: 0,
+  timeIdx: 4,
+  subjectIdx: 0,
+  classroomIdx: 15,
+  classIdx: 0,
+};
+export const cell2 = {
+  professorIdx: 0,
+  timeIdx: 1,
+  subjectIdx: 0,
+  classroomIdx: 15,
+  classIdx: 1,
+};
+export const cell3 = {
+  professorIdx: 0,
+  timeIdx: 2,
+  subjectIdx: 0,
+  classroomIdx: 15,
+  classIdx: 2,
+};
+export const cell4 = {
+  professorIdx: 0,
+  timeIdx: 3,
+  subjectIdx: 0,
+  classroomIdx: 15,
+  classIdx: 3,
+};
+export const cell5 = {
+  professorIdx: 0,
+  timeIdx: 7,
+  subjectIdx: 0,
+  classroomIdx: 15,
+  classIdx: 0,
+};
+export const cell6 = {
+  professorIdx: 0,
+  timeIdx: 8,
+  subjectIdx: 0,
+  classroomIdx: 15,
+  classIdx: 1,
+};
+export const cell7 = {
+  professorIdx: 0,
+  timeIdx: 6,
+  subjectIdx: 0,
+  classroomIdx: 15,
+  classIdx: 2,
+};
+export const cell8 = {
+  professorIdx: 0,
+  timeIdx: 10,
+  subjectIdx: 0,
+  classroomIdx: 15,
+  classIdx: 3,
+};
+export const cell9 = {
+  professorIdx: 0,
+  timeIdx: 14,
+  subjectIdx: 0,
+  classroomIdx: 15,
+  classIdx: 0,
+};
+export const cell10 = {
+  professorIdx: 0,
+  timeIdx: 15,
+  subjectIdx: 0,
+  classroomIdx: 15,
+  classIdx: 1,
+};
+export const cell11 = {
+  professorIdx: 0,
+  timeIdx: 16,
+  subjectIdx: 0,
+  classroomIdx: 15,
+  classIdx: 2,
+};
+export const cell12 = {
+  professorIdx: 0,
+  timeIdx: 17,
+  subjectIdx: 0,
+  classroomIdx: 15,
+  classIdx: 3,
+};
+export const cell13 = {
+  professorIdx: 2,
+  timeIdx: 1,
+  subjectIdx: 1,
+  classroomIdx: 17,
+  classIdx: 0,
+};
+export const cell14 = {
+  professorIdx: 2,
+  timeIdx: 5,
+  subjectIdx: 1,
+  classroomIdx: 17,
+  classIdx: 1,
+};
+export const cell15 = {
+  professorIdx: 2,
+  timeIdx: 3,
+  subjectIdx: 1,
+  classroomIdx: 17,
+  classIdx: 2,
+};
+export const cell16 = {
+  professorIdx: 2,
+  timeIdx: 4,
+  subjectIdx: 1,
+  classroomIdx: 17,
+  classIdx: 3,
+};
+export const cell17 = {
+  professorIdx: 2,
+  timeIdx: 8,
+  subjectIdx: 1,
+  classroomIdx: 17,
+  classIdx: 0,
+};
+export const cell18 = {
+  professorIdx: 2,
+  timeIdx: 9,
+  subjectIdx: 1,
+  classroomIdx: 17,
+  classIdx: 1,
+};
+export const cell19 = {
+  professorIdx: 2,
+  timeIdx: 10,
+  subjectIdx: 1,
+  classroomIdx: 17,
+  classIdx: 2,
+};
+export const cell20 = {
+  professorIdx: 2,
+  timeIdx: 11,
+  subjectIdx: 1,
+  classroomIdx: 17,
+  classIdx: 3,
+};
+export const cell21 = {
+  professorIdx: 2,
+  timeIdx: 15,
+  subjectIdx: 1,
+  classroomIdx: 17,
+  classIdx: 0,
+};
+export const cell22 = {
+  professorIdx: 2,
+  timeIdx: 16,
+  subjectIdx: 1,
+  classroomIdx: 17,
+  classIdx: 1,
+};
+export const cell23 = {
+  professorIdx: 2,
+  timeIdx: 13,
+  subjectIdx: 1,
+  classroomIdx: 17,
+  classIdx: 2,
+};
+export const cell24 = {
+  professorIdx: 2,
+  timeIdx: 14,
+  subjectIdx: 1,
+  classroomIdx: 17,
+  classIdx: 3,
+};
+export const cell25 = {
+  professorIdx: 1,
+  timeIdx: 2,
+  subjectIdx: 2,
+  classroomIdx: 9,
+  classIdx: 0,
+};
+export const cell26 = {
+  professorIdx: 1,
+  timeIdx: 3,
+  subjectIdx: 2,
+  classroomIdx: 9,
+  classIdx: 1,
+};
+export const cell27 = {
+  professorIdx: 1,
+  timeIdx: 4,
+  subjectIdx: 2,
+  classroomIdx: 9,
+  classIdx: 2,
+};
+export const cell28 = {
+  professorIdx: 1,
+  timeIdx: 5,
+  subjectIdx: 2,
+  classroomIdx: 9,
+  classIdx: 3,
+};
+export const cell29 = {
+  professorIdx: 1,
+  timeIdx: 9,
+  subjectIdx: 2,
+  classroomIdx: 9,
+  classIdx: 0,
+};
+export const cell30 = {
+  professorIdx: 1,
+  timeIdx: 10,
+  subjectIdx: 2,
+  classroomIdx: 9,
+  classIdx: 1,
+};
+export const cell31 = {
+  professorIdx: 1,
+  timeIdx: 11,
+  subjectIdx: 2,
+  classroomIdx: 9,
+  classIdx: 2,
+};
+export const cell32 = {
+  professorIdx: 1,
+  timeIdx: 7,
+  subjectIdx: 2,
+  classroomIdx: 9,
+  classIdx: 3,
+};
+export const cell33 = {
+  professorIdx: 5,
+  timeIdx: 3,
+  subjectIdx: 3,
+  classroomIdx: 20,
+  classIdx: 0,
+};
+export const cell34 = {
+  professorIdx: 5,
+  timeIdx: 4,
+  subjectIdx: 3,
+  classroomIdx: 20,
+  classIdx: 1,
+};
+export const cell35 = {
+  professorIdx: 5,
+  timeIdx: 5,
+  subjectIdx: 3,
+  classroomIdx: 20,
+  classIdx: 2,
+};
+export const cell36 = {
+  professorIdx: 5,
+  timeIdx: 2,
+  subjectIdx: 3,
+  classroomIdx: 20,
+  classIdx: 3,
+};
+export const cell37 = {
+  professorIdx: 5,
+  timeIdx: 10,
+  subjectIdx: 3,
+  classroomIdx: 20,
+  classIdx: 0,
+};
+export const cell38 = {
+  professorIdx: 5,
+  timeIdx: 11,
+  subjectIdx: 3,
+  classroomIdx: 20,
+  classIdx: 1,
+};
+export const cell39 = {
+  professorIdx: 5,
+  timeIdx: 8,
+  subjectIdx: 3,
+  classroomIdx: 20,
+  classIdx: 2,
+};
+export const cell40 = {
+  professorIdx: 5,
+  timeIdx: 9,
+  subjectIdx: 3,
+  classroomIdx: 20,
+  classIdx: 3,
+};
+export const cell41 = {
+  professorIdx: 8,
+  timeIdx: 0,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 0,
+};
+export const cell42 = {
+  professorIdx: 8,
+  timeIdx: 1,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 3,
+};
+export const cell43 = {
+  professorIdx: 8,
+  timeIdx: 2,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 1,
+};
+export const cell44 = {
+  professorIdx: 8,
+  timeIdx: 6,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 0,
+};
+export const cell45 = {
+  professorIdx: 8,
+  timeIdx: 7,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 2,
+};
+export const cell46 = {
+  professorIdx: 8,
+  timeIdx: 8,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 3,
+};
+export const cell47 = {
+  professorIdx: 8,
+  timeIdx: 13,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 0,
+};
+export const cell48 = {
+  professorIdx: 8,
+  timeIdx: 14,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 1,
+};
+export const cell49 = {
+  professorIdx: 8,
+  timeIdx: 12,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 2,
+};
+export const cell50 = {
+  professorIdx: 8,
+  timeIdx: 19,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 3,
+};
+export const cell51 = {
+  professorIdx: 8,
+  timeIdx: 29,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 0,
+};
+export const cell52 = {
+  professorIdx: 8,
+  timeIdx: 21,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 1,
+};
+export const cell53 = {
+  professorIdx: 8,
+  timeIdx: 22,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 2,
+};
+
+export const cell54 = {
+  professorIdx: 8,
+  timeIdx: 26,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 1,
+};
+export const cell55 = {
+  professorIdx: 8,
+  timeIdx: 27,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 2,
+};
+
+export const cell56 = {
+  professorIdx: 8,
+  timeIdx: 28,
+  subjectIdx: 4,
+  classroomIdx: 0,
+  classIdx: 3,
+};
+export const cell57 = {
+  professorIdx: 3,
+  timeIdx: 12,
+  subjectIdx: 5,
+  classroomIdx: 7,
+  classIdx: 0,
+};
+export const cell58 = {
+  professorIdx: 3,
+  timeIdx: 13,
+  subjectIdx: 5,
+  classroomIdx: 7,
+  classIdx: 1,
+};
+export const cell59 = {
+  professorIdx: 3,
+  timeIdx: 14,
+  subjectIdx: 5,
+  classroomIdx: 7,
+  classIdx: 2,
+};
+export const cell60 = {
+  professorIdx: 3,
+  timeIdx: 15,
+  subjectIdx: 5,
+  classroomIdx: 7,
+  classIdx: 3,
+};
+export const cell61 = {
+  professorIdx: 3,
+  timeIdx: 20,
+  subjectIdx: 5,
+  classroomIdx: 7,
+  classIdx: 0,
+};
+export const cell62 = {
+  professorIdx: 3,
+  timeIdx: 22,
+  subjectIdx: 5,
+  classroomIdx: 7,
+  classIdx: 1,
+};
+export const cell63 = {
+  professorIdx: 3,
+  timeIdx: 21,
+  subjectIdx: 5,
+  classroomIdx: 7,
+  classIdx: 2,
+};
+export const cell64 = {
+  professorIdx: 3,
+  timeIdx: 18,
+  subjectIdx: 5,
+  classroomIdx: 7,
+  classIdx: 3,
+};
+export const cell65 = {
+  professorIdx: 4,
+  timeIdx: 12,
+  subjectIdx: 13,
+  classroomIdx: 5,
+  classIdx: 1,
+};
+export const cell66 = {
+  professorIdx: 4,
+  timeIdx: 17,
+  subjectIdx: 13,
+  classroomIdx: 5,
+  classIdx: 2,
+};
+export const cell67 = {
+  professorIdx: 4,
+  timeIdx: 13,
+  subjectIdx: 13,
+  classroomIdx: 5,
+  classIdx: 3,
+};
+export const cell68 = {
+  professorIdx: 4,
+  timeIdx: 16,
+  subjectIdx: 13,
+  classroomIdx: 5,
+  classIdx: 0,
+};
+export const cell69 = {
+  professorIdx: 4,
+  timeIdx: 18,
+  subjectIdx: 13,
+  classroomIdx: 5,
+  classIdx: 0,
+};
+export const cell70 = {
+  professorIdx: 4,
+  timeIdx: 19,
+  subjectIdx: 13,
+  classroomIdx: 5,
+  classIdx: 1,
+};
+export const cell71 = {
+  professorIdx: 4,
+  timeIdx: 20,
+  subjectIdx: 13,
+  classroomIdx: 5,
+  classIdx: 2,
+};
+export const cell72 = {
+  professorIdx: 4,
+  timeIdx: 21,
+  subjectIdx: 13,
+  classroomIdx: 5,
+  classIdx: 3,
+};
+export const cell73 = {
+  professorIdx: 6,
+  timeIdx: 24,
+  subjectIdx: 6,
+  classroomIdx: 3,
+  classIdx: 0,
+};
+export const cell74 = {
+  professorIdx: 6,
+  timeIdx: 25,
+  subjectIdx: 6,
+  classroomIdx: 3,
+  classIdx: 1,
+};
+export const cell75 = {
+  professorIdx: 6,
+  timeIdx: 26,
+  subjectIdx: 6,
+  classroomIdx: 3,
+  classIdx: 2,
+};
+export const cell76 = {
+  professorIdx: 6,
+  timeIdx: 27,
+  subjectIdx: 6,
+  classroomIdx: 3,
+  classIdx: 3,
+};
+export const cell77 = {
+  professorIdx: 7,
+  timeIdx: 0,
+  subjectIdx: 10,
+  classroomIdx: 8,
+  classIdx: 1,
+};
+export const cell78 = {
+  professorIdx: 7,
+  timeIdx: 1,
+  subjectIdx: 10,
+  classroomIdx: 8,
+  classIdx: 2,
+};
+export const cell79 = {
+  professorIdx: 7,
+  timeIdx: 16,
+  subjectIdx: 10,
+  classroomIdx: 8,
+  classIdx: 3,
+};
+export const cell80 = {
+  professorIdx: 7,
+  timeIdx: 17,
+  subjectIdx: 10,
+  classroomIdx: 8,
+  classIdx: 0,
+};
+export const cell81 = {
+  professorIdx: 7,
+  timeIdx: 18,
+  subjectIdx: 10,
+  classroomIdx: 8,
+  classIdx: 1,
+};
+export const cell82 = {
+  professorIdx: 7,
+  timeIdx: 19,
+  subjectIdx: 10,
+  classroomIdx: 8,
+  classIdx: 2,
+};
+export const cell83 = {
+  professorIdx: 7,
+  timeIdx: 20,
+  subjectIdx: 10,
+  classroomIdx: 8,
+  classIdx: 3,
+};
+export const cell84 = {
+  professorIdx: 7,
+  timeIdx: 21,
+  subjectIdx: 10,
+  classroomIdx: 8,
+  classIdx: 0,
+};
+export const cell85 = {
+  professorIdx: 7,
+  timeIdx: 25,
+  subjectIdx: 10,
+  classroomIdx: 8,
+  classIdx: 0,
+};
+export const cell86 = {
+  professorIdx: 7,
+  timeIdx: 26,
+  subjectIdx: 10,
+  classroomIdx: 8,
+  classIdx: 3,
+};
+export const cell87 = {
+  professorIdx: 7,
+  timeIdx: 27,
+  subjectIdx: 10,
+  classroomIdx: 8,
+  classIdx: 1,
+};
+export const cell88 = {
+  professorIdx: 7,
+  timeIdx: 28,
+  subjectIdx: 10,
+  classroomIdx: 8,
+  classIdx: 2,
+};
+export const cell89 = {
+  professorIdx: 9,
+  timeIdx: 6,
+  subjectIdx: 9,
+  classroomIdx: 13,
+  classIdx: 3,
+};
+export const cell90 = {
+  professorIdx: 9,
+  timeIdx: 7,
+  subjectIdx: 9,
+  classroomIdx: 13,
+  classIdx: 1,
+};
+export const cell91 = {
+  professorIdx: 9,
+  timeIdx: 9,
+  subjectIdx: 9,
+  classroomIdx: 13,
+  classIdx: 2,
+};
+export const cell92 = {
+  professorIdx: 9,
+  timeIdx: 24,
+  subjectIdx: 9,
+  classroomIdx: 13,
+  classIdx: 3,
+};
+export const cell93 = {
+  professorIdx: 9,
+  timeIdx: 18,
+  subjectIdx: 9,
+  classroomIdx: 13,
+  classIdx: 2,
+};
+export const cell94 = {
+  professorIdx: 9,
+  timeIdx: 19,
+  subjectIdx: 9,
+  classroomIdx: 13,
+  classIdx: 0,
+};
+export const cell95 = {
+  professorIdx: 9,
+  timeIdx: 20,
+  subjectIdx: 9,
+  classroomIdx: 13,
+  classIdx: 1,
+};
+export const cell96 = {
+  professorIdx: 9,
+  timeIdx: 25,
+  subjectIdx: 9,
+  classroomIdx: 13,
+  classIdx: 3,
+};
+export const cell97 = {
+  professorIdx: 9,
+  timeIdx: 26,
+  subjectIdx: 9,
+  classroomIdx: 13,
+  classIdx: 0,
+};
+export const cell98 = {
+  professorIdx: 9,
+  timeIdx: 27,
+  subjectIdx: 9,
+  classroomIdx: 13,
+  classIdx: 0,
+};
+export const cell99 = {
+  professorIdx: 9,
+  timeIdx: 28,
+  subjectIdx: 9,
+  classroomIdx: 13,
+  classIdx: 1,
+};
+export const cell100 = {
+  professorIdx: 9,
+  timeIdx: 29,
+  subjectIdx: 9,
+  classroomIdx: 13,
+  classIdx: 2,
+};
+export const cell101 = {
+  professorIdx: 6,
+  timeIdx: 28,
+  subjectIdx: 7,
+  classroomIdx: 3,
+  classIdx: 0,
+};
+export const cell102 = {
+  professorIdx: 7,
+  timeIdx: 29,
+  subjectIdx: 7,
+  classroomIdx: 8,
+  classIdx: 1,
+};
+export const cell103 = {
+  professorIdx: 4,
+  timeIdx: 15,
+  subjectIdx: 7,
+  classroomIdx: 5,
+  classIdx: 2,
+};
+export const cell104 = {
+  professorIdx: 2,
+  timeIdx: 0,
+  subjectIdx: 7,
+  classroomIdx: 17,
+  classIdx: 3,
+};
+
+export const tempSolution2 = [cell1, cell2, cell3, cell4];
---
+++ src/utils/helper-functions.js
@@ -0,0 +1,402 @@
+import {
+  professors,
+  subjects,
+  time,
+  classrooms,
+  classes,
+  cell1,
+  cell2,
+  cell3,
+  cell4,
+  cell5,
+  cell6,
+  cell7,
+  cell8,
+  cell9,
+  cell10,
+  cell11,
+  cell12,
+  cell13,
+  cell14,
+  cell15,
+  cell16,
+  cell17,
+  cell18,
+  cell19,
+  cell20,
+  cell21,
+  cell22,
+  cell23,
+  cell24,
+  cell25,
+  cell26,
+  cell27,
+  cell28,
+  cell29,
+  cell30,
+  cell31,
+  cell32,
+  cell33,
+  cell34,
+  cell35,
+  cell36,
+  cell37,
+  cell38,
+  cell39,
+  cell40,
+  cell41,
+  cell42,
+  cell43,
+  cell44,
+  cell45,
+  cell46,
+  cell47,
+  cell48,
+  cell49,
+  cell50,
+  cell51,
+  cell52,
+  cell53,
+  cell54,
+  cell55,
+  cell56,
+  cell57,
+  cell58,
+  cell59,
+  cell60,
+  cell61,
+  cell62,
+  cell63,
+  cell64,
+  cell65,
+  cell66,
+  cell67,
+  cell68,
+  cell69,
+  cell70,
+  cell71,
+  cell72,
+  cell73,
+  cell74,
+  cell75,
+  cell76,
+  cell77,
+  cell78,
+  cell79,
+  cell80,
+  cell81,
+  cell82,
+  cell83,
+  cell84,
+  cell85,
+  cell86,
+  cell87,
+  cell88,
+  cell89,
+  cell90,
+  cell91,
+  cell92,
+  cell93,
+  cell94,
+  cell95,
+  cell96,
+  cell97,
+  cell98,
+  cell99,
+  cell100,
+  cell101,
+  cell102,
+  cell103,
+  cell104,
+} from "../utils/data";
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////    HELPER FUNCTIONS    /////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+//  Function that checks if array has breaks between class times
+
+//  checkForBreakBetweenClasses
+// Returns 1 if theres a break
+// returns -1 if there is no break
+// return 0 if its different day
+
+export const checkIfClassDayIsContinous = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let hasBreak = checkForBreakBetweenClasses(arr[i + 1], arr[i]);
+    if (hasBreak == 1) {
+      if (hasBreak) return false; // we found a break
+    } else if (hasBreak == -1 || hasBreak == 0) {
+      continue;
+    }
+  }
+  return true;
+};
+
+// export const checkIfProfessorDayIsContinousOrWithOneBreak = (arr) => {
+//   let foundOneBreakInOneDay = false;
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     if (arr[i + 1][0] == arr[i][0]) {
+//       // If its still the same day m == m for example (monday == monday)
+//       if (arr[i + 1][1] - arr[i][1] > 1) {
+//         return false;
+//       } else if (arr[i + 1][1] - arr[i][1] == 1) {
+//         if (foundOneBreakInOneDay === true) {
+//           //  We found another break in one day
+//           return false;
+//         }
+//         foundOneBreakInOneDay = true;
+//         continue;
+//       } else {
+//         continue;
+//       }
+//     } else {
+//       foundOneBreakInOneDay = false; // We switched from one day to another so we need to restart the variable for checking one break a day
+//       continue;
+//     }
+//   }
+// };
+
+//  checkForBreakBetweenClasses
+// Returns 1 if theres a break
+// returns -1 if there is no break
+// return 0 if its different day
+
+export const checkIfProfessorDayIsContinousOrWithOneBreak = (arr) => {
+  let foundOneBreakInOneDay = false;
+  for (let i = 0; i < arr.length - 1; i++) {
+    let hasBreak = checkForBreakBetweenClasses(arr[i + 1], arr[i]);
+    if (hasBreak == 1) {
+      if (foundOneBreakInOneDay) return false; // we found second break in 1 day
+      foundOneBreakInOneDay = true;
+    } else if (hasBreak == -1) {
+      continue;
+    } else if (hasBreak == 0) {
+      foundOneBreakInOneDay = false;
+    }
+  }
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//  Function to check if some professor is in different classes at the same time
+//  Solution is made of 'solutionCells', we have to check if some two cells have the
+//  the same attributes professorIdx and timeIdx which would mean that the same professor
+//  is asigned to different class at the same time
+export const checkForSameProfessorDifferentClass = (solution) => {
+  //  we will accumulate penalties in this variable
+  let sum = 0;
+
+  //    we will iterate through the solution(all cells) for all professors and check if the times collide
+  for (let i = 0; i < professors.length; i++) {
+    let timesForProfessor = new Array(time.length).fill(false);
+    for (let j = 0; j < solution.length; j++) {
+      // console.log("s", solution[j]);
+      if (
+        solution[j].professorIdx === i &&
+        timesForProfessor[solution[j].timeIdx] === true
+      ) {
+        sum -= 10000; //    We found a cell in which is professor assigned to a time in which he is already assigned
+      } else if (
+        solution[j].professorIdx === i &&
+        timesForProfessor[solution[j].timeIdx] === false
+      ) {
+        timesForProfessor[solution[j].timeIdx] = true;
+      } else {
+        continue;
+      }
+    }
+    // console.log("times for proff", timesForProfessor);
+  }
+  return sum; //   We didnt find double times for the same professor
+};
+
+export const checkForSameClassDifferentClass = (solution) => {
+  //  we will accumulate penalties in this variable
+  let sum = 0;
+
+  //    we will iterate through the solution(all cells) for all classes and check if the times collide
+  for (let i = 0; i < classes.length; i++) {
+    let timesForClass = new Array(time.length).fill(false);
+    for (let j = 0; j < solution.length; j++) {
+      // console.log("s", solution[j]);
+      if (
+        solution[j].classIdx === i &&
+        timesForClass[solution[j].timeIdx] === true
+      ) {
+        console.log(solution[j].classIdx, solution[j].timeIdx, j);
+        sum -= 10000; //    We found a cell in which is class assigned to a time in which they are already assigned
+      } else if (
+        solution[j].classIdx === i &&
+        timesForClass[solution[j].timeIdx] === false
+      ) {
+        timesForClass[solution[j].timeIdx] = true;
+      } else {
+        continue;
+      }
+    }
+    console.log("times for class", timesForClass);
+  }
+  return sum; //   We didnt find double times for the same calss
+};
+
+//  Function checks if some class has all their subjects one after another,
+//  without time gaps of 45mins or more and have between 4 and 7 classes a day
+export const checkClassGapsAndNumOfLessons = (solution) => {
+  //  we will accumulate penalties in this variable
+  let sum = 0;
+
+  //    we will iterate through the solution(all cells) for all classes and check if one class has continous lessons in a day
+  for (let i = 0; i < classes.length; i++) {
+    let classesPerDay = new Array(5).fill(0); //    Array of zeros for each day from monday to friday, when we find a solution cell for we increment that index in an array
+    let allClassTimesFound = new Array(); // We will fill this array with all times found for current class in current solution, then sort it and check if the times for each day are continous
+    //  Could optimize this to make it instead of going through the solution for each class, check for all classes in one going by putting
+    //  variables 'classesPerDay and allClassTimesFound in an array, where array is slot for each class to check
+    for (let j = 0; j < solution.length; j++) {
+      if (solution[j].classIdx === i) {
+        if (time[solution[j].timeIdx][0] == "M") {
+          classesPerDay[0]++;
+          allClassTimesFound.push(time[solution[j].timeIdx]);
+        } else if (time[solution[j].timeIdx][0] == "T") {
+          if (time[solution[j].timeIdx][1] != "H") {
+            classesPerDay[1]++;
+            allClassTimesFound.push(time[solution[j].timeIdx]);
+          } else {
+            classesPerDay[3]++;
+            allClassTimesFound.push(time[solution[j].timeIdx]);
+          }
+        } else if (time[solution[j].timeIdx][0] == "W") {
+          classesPerDay[2]++;
+          allClassTimesFound.push(time[solution[j].timeIdx]);
+        } else if (time[solution[j].timeIdx][0] == "F") {
+          classesPerDay[4]++;
+          allClassTimesFound.push(time[solution[j].timeIdx]);
+        }
+      } else {
+        continue;
+      }
+    }
+    for (let j = 0; j < 5; j++) {
+      if (classesPerDay[j] < 4 || classesPerDay[j] > 7) {
+        sum -= 5000;
+        break;
+      }
+    }
+    allClassTimesFound.sort();
+    // console.log("all class times found", allClassTimesFound);
+    if (checkIfClassDayIsContinous(allClassTimesFound) == false) {
+      sum -= 10000;
+    }
+  }
+  return sum; //   We didnt find double times for the same professor
+};
+
+//  Functions that checks if professor has at most one break of 45 mins,
+//  a 'one lesson' gap and between 2 and 7 classes a day
+export const checkProfessorBreakAndNumOfLessons = (solution) => {
+  //    we will iterate through the solution(all cells) for all professors and check if one professor has at most
+  //    1 break of 45 mins between classes in a day and has between 2 and 7 lessons a day
+
+  let sum = 0; // Accumulate the penalties in this variable;
+  for (let i = 0; i < professors.length; i++) {
+    let classesPerDay = new Array(5).fill(0); //    Array of zeros for each day from monday to friday, when we find a solution cell for we increment that index in an array
+    let allClassTimesFound = new Array(); // We will fill this array with all times found for current professor in current solution, then sort it and check if the times for each day are continous
+    //  or with one break at most
+    //  Could optimize this to make it instead of going through the solution for each professor, check for all professor in one going by putting
+    //  variables 'classesPerDay and allClassTimesFound in an array, where array is slot for each class to check
+    for (let j = 0; j < solution.length; j++) {
+      if (solution[j].professorIdx === i) {
+        if (time[solution[j].timeIdx][0] == "M") {
+          classesPerDay[0]++;
+          allClassTimesFound.push(time[solution[j].timeIdx]);
+        } else if (time[solution[j].timeIdx][0] == "T") {
+          if (time[solution[j].timeIdx][1] != "H") {
+            classesPerDay[1]++;
+            allClassTimesFound.push(time[solution[j].timeIdx]);
+          } else {
+            classesPerDay[3]++;
+            allClassTimesFound.push(time[solution[j].timeIdx]);
+          }
+        } else if (time[solution[j].timeIdx][0] == "W") {
+          classesPerDay[2]++;
+          allClassTimesFound.push(time[solution[j].timeIdx]);
+        } else if (time[solution[j].timeIdx][0] == "F") {
+          classesPerDay[4]++;
+          allClassTimesFound.push(time[solution[j].timeIdx]);
+        }
+      } else {
+        continue;
+      }
+    }
+    for (let j = 0; j < 5; j++) {
+      if (classesPerDay[j] == 1 || classesPerDay[j] > 7) {
+        sum -= 5000;
+        break;
+      }
+    }
+    allClassTimesFound.sort();
+    // console.log("all class time founds", allClassTimesFound);
+    if (
+      checkIfProfessorDayIsContinousOrWithOneBreak(allClassTimesFound) == false
+    ) {
+      sum -= 10;
+    }
+  }
+  return sum; //   We didnt find double times for the same professor
+};
+
+export function getRdn(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+export function checkBound(val, lower, upper) {
+  if (val < lower) return lower;
+  if (val > upper) return upper;
+  return val;
+}
+
+export function average(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum / array.length;
+}
+
+// Function to compare class times and check for breaks within the same day
+// Returns 1 if theres a break
+// returns -1 if there is no break
+// return 0 if its different day
+function checkForBreakBetweenClasses(time1, time2) {
+  const daysOfWeek = ["M", "TU", "W", "TH", "F"];
+
+  const getDayIndex = (time) => {
+    const day = time.substring(0, time.length == 2 ? 1 : 2); // Consider both one and two-letter days
+    return daysOfWeek.indexOf(day);
+  };
+
+  const getTimeIndex = (time) => {
+    let substringIdx = time.length == 2 ? 1 : 2;
+    return parseInt(time.substring(substringIdx));
+  };
+
+  const dayIndex1 = getDayIndex(time1);
+  const dayIndex2 = getDayIndex(time2);
+
+  // If times are on different days
+  if (dayIndex1 !== dayIndex2) {
+    return 0;
+  }
+
+  const timeIndex1 = getTimeIndex(time1);
+  const timeIndex2 = getTimeIndex(time2);
+
+  // Check if there's a break between classes on the same day
+  if (timeIndex2 - timeIndex1 > 1) {
+    return 1; // There's a break between classes
+  }
+
+  return -1; // No break between classes
+}
