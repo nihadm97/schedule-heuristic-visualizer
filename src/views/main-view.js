b288887095303694f4fac7fea6fba6aac03f88be
Zinedin Kadric <zinedin_kadric@hotmail.com>
2023-12-22 15:02:06 +0100

    Resolved conflicts

--- src/views/main-view.js
+++ src/views/main-view.js
@@ -1576,192 +1576,27 @@ function optimizeScheduleByProfessor(initialSolution, setTempSolution) {
   return schedule;
 }
 
-function getRdn(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-function checkBound(val, lower, upper) {
-  if (val < lower) return lower;
-  if (val > upper) return upper;
-  return val;
-}
-
-function average(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
-  return sum / array.length;
-}
-
-// Define the cost function
-function cost(x) {
-  // temporaly cost function
-  let sum = 0;
-  for (let i = 0; i < x.length; i++) {
-    sum += x[i].timeIdx * x[i].timeIdx;
-  }
-  return sum;
-}
-
-// console.log("test", cost(tempSolution), tempSolution);
-
-function cost_2(x) {
-  // reference error in checkProfessorBreakAndNumOfLessons on Line 288, needs to be resolved
-  let sum = 0;
-  sum += checkProfessorBreakAndNumOfLessons(x);
-  console.log(sum);
-  sum += checkForSameProfessorDifferentClass(x);
-  console.log(sum);
-
-  sum += checkClassGapsAndNumOfLessons(x);
-  console.log(sum);
-
-  return sum;
-}
-
-console.log("test 2", cost_2(tempSolution2), tempSolution2);
-
-function switchTimes(arrayTimes, arraySolution) {
-  // switch old timeslots with new timeslots
-  for (let i = 0; i < arrayTimes.length; i++) {
-    arraySolution[i].timeIdx = arrayTimes[i];
-  }
-  return arraySolution;
-}
+// console.log("test 2", cost_2(tempSolution2), tempSolution2);
 
 // bat algorithm for finding best timeslots for tempSolution, rewrites the tempSolution with new timeslots
 // bestBat variable includes best timeslots for tempSolution
-function batAlgorithm(
-  costFunc,
-  saveRate = 100,
-  maxGen = 1000,
-  popSize = 50,
-  solution = tempSolution,
-  maxLoudness = 2,
-  maxPulseRate = 1,
-  fMin = 0,
-  fMax = 10,
-  lowerBound = 0,
-  upperBound = time.length
-) {
-  if (!costFunc)
-    throw new Error(
-      "Please pass a valid cost function for your optimization problems"
-    );
-  const ALPHA = 0.9;
-  const GAMMA = 0.9;
-
-  // parameters initialization using uniform distribution
-  let position = [];
-  let velocity = [];
-  let loudness = [];
-  let pulseRate = [];
-  let frequency = [];
-  let newPosition = []; // bats local array used to calculate bats new position at each generation, before acceptation
-
-  for (let i = 0; i < popSize; i++) {
-    loudness[i] = getRdn(1, maxLoudness);
-    pulseRate[i] = getRdn(0, maxPulseRate);
-    frequency[i] = getRdn(fMin, fMax);
-    position[i] = [];
-    velocity[i] = [];
-    newPosition[i] = [];
-
-    for (let j = 0; j < solution.length; j++) {
-      position[i][j] = Math.round(getRdn(lowerBound, upperBound));
-      velocity[i][j] = getRdn(lowerBound, upperBound);
-    }
-  }
-
-  // evaluate the bats after initialization
-  let cost = [];
-  for (let i = 0; i < popSize; i++) {
-    cost[i] = costFunc(switchTimes(position[i], solution));
-  }
-
-  let bestBat;
-  // cycle through each generation
-  for (let gen = 1; gen <= maxGen; gen++) {
-    let indexMin = cost.indexOf(Math.min(...cost)); // best bat index so far
-    bestBat = position[indexMin]; // best bat so far
-
-    if (gen % saveRate === 0 || gen === 1) {
-      let data = {};
-
-      data.gen = gen;
-      data.loudness = loudness;
-      data.pulseRate = pulseRate;
-      data.frequency = frequency;
-      data.position = position;
-      data.velocity = velocity;
-      data.bestBat = bestBat;
-    }
-
-    // cycle through the population
-    for (let i = 0; i < popSize; i++) {
-      // getRdn(0,1) = beta
-      frequency[i] = fMin + (fMax - fMin) * getRdn(0, 1); // assign new frequency to each bat (2)
-      for (let j = 0; j < solution.length; j++) {
-        velocity[i][j] =
-          velocity[i][j] + (position[i][j] - bestBat[j]) * frequency[i]; // assign new velocity to each bat (3)
-        newPosition[i][j] = Math.round(position[i][j] + velocity[i][j]); // creating a new position that is not yet assigned to the bat (4)
-        newPosition[i][j] = checkBound(
-          newPosition[i][j],
-          lowerBound,
-          upperBound
-        );
-      }
-
-      // generate a local solution around the best solution
-      if (getRdn(0, 1) > pulseRate[i]) {
-        for (let j = 0; j < solution.length; j++) {
-          // getRdn(-1, 1) = epsilon
-          newPosition[i][j] = Math.round(
-            bestBat[j] + getRdn(-1, 1) * average(loudness)
-          ); // random walk (5)
-          newPosition[i][j] = checkBound(
-            newPosition[i][j],
-            lowerBound,
-            upperBound
-          );
-        }
-      }
-
-      let newCost = costFunc(switchTimes(newPosition[i], solution)); // bat 'newPosition's cost
-
-      // try to accept the new solution
-      if (getRdn(0, 1) < loudness[i] || newCost <= cost[i]) {
-        // new solution accepted, assigning new position to each bat
-        for (let j = 0; j < solution.length; j++) {
-          position[i][j] = newPosition[i][j];
-        }
-        cost[i] = newCost;
-        loudness[i] = loudness[i] * ALPHA; // loudness update (6)
-        pulseRate[i] = pulseRate[i] * (1 - Math.exp(-GAMMA * gen)); // pulse rate update (6)
-      }
-    }
-  }
-  switchTimes(bestBat, solution);
-  return solution;
-}
 
 // Call the batAlgorithm() function with optional parameters
 //const result = batAlgorithm(cost);
-const result = batAlgorithm(
-  cost,
-  150,
-  10000,
-  100,
-  tempSolution,
-  2,
-  1,
-  -10,
-  10,
-  0,
-  time.length
-);
-console.log(result);
+// const result = batAlgorithm(
+//   cost,
+//   150,
+//   10000,
+//   100,
+//   tempSolution,
+//   2,
+//   1,
+//   -10,
+//   10,
+//   0,
+//   time.length
+// );
+// console.log(result);
 
 export default function MainView() {
   const renderCell = (timeslotIndex, row) => {
